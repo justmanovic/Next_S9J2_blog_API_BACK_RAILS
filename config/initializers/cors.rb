51be22b3b409eaa8e029139d8eaaaeccb3ba0c51
jonathan justman <jojo.justman@gmail.com>
2021-11-23 15:32:57 +0100

    signup et login ok

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
     resource '*',
-             headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      headers: %w(Authorization),
+      methods: :any,
+      expose: %w(Authorization),
+      max_age: 600
   end
 end
